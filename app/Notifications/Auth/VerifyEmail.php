7a8bb42d63043d2409daa50ac430cb5ff732a7f6
Chris Vince <chrisvince@gmail.com>
2019-02-02 16:46:43 -0500

    Added password reset and email verification functionality
    • Added routes to api.php to allow for password reset and email
      verification functionality
    • Updated User.php to include functions for password reset and
      email verification
    • Created app/Notifications/Auth/ResetPassword.php with customization
    • Created app/Notifications/Auth/VerifyEmail.php with customization
    • Registered Illuminate\Notifications\NotificationServiceProvider in app.php
    • Created migration for password_resets table
    • Created migration for failed-jobs table
    • Created migration for email-verification table
    • Added email_verified_at column to users table

---
+++ app/Notifications/Auth/VerifyEmail.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Notifications\Auth;
+
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Notifications\Notification;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class VerifyEmail extends Notification implements ShouldQueue
+{
+	use Queueable;
+	
+    /**
+     * The verify email token.
+     *
+     * @var string
+     */
+    public $token;
+    
+    /**
+     * The callback that should be used to build the mail message.
+     *
+     * @var \Closure|null
+     */
+    public static $toMailCallback;
+    
+    /**
+     * Create a notification instance.
+     *
+     * @param  string  $token
+     * @return void
+     */
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+    
+    /**
+     * Get the notification's channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array|string
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Build the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        if (static::$toMailCallback) {
+            return call_user_func(static::$toMailCallback, $notifiable);
+        }
+
+        return (new MailMessage)->view(
+            'emails.auth.verify-email', [
+                'token' => $this->token,
+                'email' => $notifiable->email
+            ]
+        );
+    }
+
+    /**
+     * Get the verification URL for the given notifiable.
+     *
+     * @param  mixed  $notifiable
+     * @return string
+     */
+    protected function verificationUrl($notifiable)
+    {
+        return URL::temporarySignedRoute(
+            'verification.verify',
+            Carbon::now()->addMinutes(Config::get('auth.verification.expire', 60)),
+            ['id' => $notifiable->getKey()]
+        );
+    }
+
+    /**
+     * Set a callback that should be used when building the notification mail message.
+     *
+     * @param  \Closure  $callback
+     * @return void
+     */
+    public static function toMailUsing($callback)
+    {
+        static::$toMailCallback = $callback;
+    }
+}
---
+++ resources/views/emails/auth/reset-password.blade.php
@@ -0,0 +1,1 @@
+Your password reset token is: {{$token}}
